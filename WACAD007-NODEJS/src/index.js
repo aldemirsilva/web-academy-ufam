0d5043a8543bf171d2925230f21517b82d8cc152
Aldemir Rodrigues da Silva <aldemir.rodrigues@gmail.com>
2026-05-22 12:44:33 -0400

    chore:(WACAD007): Adiciona exercício 1 - parte 5.

--- WACAD007-NODEJS/src/index.js
+++ WACAD007-NODEJS/src/index.js
@@ -2,14 +2,17 @@ const fs = require("fs");
 const http = require("http");
 const dotenv = require("dotenv");
 
-dotenv.config({ quiet: true });
+dotenv.config({
+  quiet: true,
+  path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+});
 
 const FOLDER = process.argv[2];
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  fs.readdir(FOLDER, (err, files) => {
-    res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
+  res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
+  fs.readdir(`${FOLDER}`, (err, files) => {
     files.forEach((f) => res.write(`${f}<br>`));
     console.log(files);
     res.end();
