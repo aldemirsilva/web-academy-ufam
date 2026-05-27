cb91051058350559b8e04b693d74237b3a9a6d44
Aldemir Rodrigues da Silva <aldemir.rodrigues@gmail.com>
2026-05-26 20:13:38 -0400

    chore(WACAD009): adiciona eercício 2 -parte 1

--- WACAD009-EXPRESS/ExpTS/src/index.ts
+++ WACAD009-EXPRESS/ExpTS/src/index.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config({
   quiet: true,
@@ -9,7 +9,7 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
