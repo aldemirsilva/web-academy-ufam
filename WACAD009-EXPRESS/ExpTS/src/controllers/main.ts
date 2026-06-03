a358bafdb09a57d3e699c92ba393fe907bbcbbc7
Aldemir Rodrigues da Silva <aldemir.rodrigues@gmail.com>
2026-06-03 14:16:15 -0400

    chore(WACAD009): adiciona exercício 2 - parte 8.

---
+++ WACAD009-EXPRESS/ExpTS/src/controllers/main.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from "express";
+import { loremIpsum } from "lorem-ipsum";
+
+const index = (req: Request, res: Response) => {
+  res.send("Hello World!");
+};
+
+const about = (req: Request, res: Response) => {
+  res.send("Pagina sobre");
+};
+
+const welcome = (req: Request, res: Response) => {
+  const nome = req.params.nome;
+  res.send(`Seja bem-vindo(a), ${nome}!`);
+};
+
+const lorem = (req: Request, res: Response) => {
+  const lorem = loremIpsum({
+    count: parseInt(req.params.paragrafos as string),
+    format: "html",
+    paragraphLowerBound: 3,
+    paragraphUpperBound: 7,
+    random: Math.random,
+    sentenceLowerBound: 5,
+    sentenceUpperBound: 15,
+    suffix: "\n",
+    units: "paragraphs",
+  });
+  res.send(lorem);
+};
+
+const hb1 = (req: Request, res: Response) => {
+  const message = "Bem-vindo(a) ao Web Academy!";
+  res.render("hb1", {
+    message,
+  });
+};
+
+const hb2 = (req: Request, res: Response) => {
+  res.render("hb2", {
+    poweredByNodeJs: true,
+    name: "Express",
+    type: "Framework",
+    message: "A mensagem não será exibida!",
+  });
+};
+
+const hb3 = (req: Request, res: Response) => {
+  const profs = [
+    { nome: "David Fernandes", sala: 1238 },
+    { nome: "Horácio Fernades", sala: 1233 },
+    { nome: "Edleno Moura", sala: 1236 },
+    { nome: "Elaine Harada", sala: 1231 },
+  ];
+  res.render("hb3", {
+    profs,
+  });
+};
+
+const hb4 = (req: Request, res: Response) => {
+  const technologies = [
+    { name: "Express", type: "Framework", poweredByNodeJs: true },
+    { name: "Laravel", type: "Framework", poweredByNodeJs: false },
+    { name: "React", type: "Library", poweredByNodeJs: true },
+    { name: "Handlebars", type: "Engine View", poweredByNodeJs: true },
+    { name: "Django", type: "Framework", poweredByNodeJs: false },
+    { name: "Docker", type: "Virtualization", poweredByNodeJs: false },
+    { name: "Sequelize", type: "ORM tool", poweredByNodeJs: true },
+  ];
+  res.render("hb4", {
+    technologies,
+  });
+};
+
+const not_found = (req: Request, res: Response) => {
+  res.statusCode = 404;
+  res.send("Erro 404: Página não encontrada.");
+};
+
+export default { index, about, welcome, lorem, hb1, hb2, hb3, hb4, not_found };
--- WACAD009-EXPRESS/ExpTS/src/router/router.ts
+++ WACAD009-EXPRESS/ExpTS/src/router/router.ts
@@ -1,86 +1,16 @@
 import Router from "express";
-import { loremIpsum } from "lorem-ipsum";
+import MainController from "../controllers/main";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-router.get("/about", (req, res) => {
-  res.send("Pagina sobre");
-});
-
-router.get("/bem-vindo/:nome", (req, res) => {
-  const nome = req.params.nome;
-  res.send(`Seja bem-vindo(a), ${nome}!`);
-});
-
-router.get("/lorem/:paragrafos", (req, res) => {
-  const lorem = loremIpsum({
-    count: parseInt(req.params.paragrafos),
-    format: "html",
-    paragraphLowerBound: 3,
-    paragraphUpperBound: 7,
-    random: Math.random,
-    sentenceLowerBound: 5,
-    sentenceUpperBound: 15,
-    suffix: "\n",
-    units: "paragraphs",
-  });
-  res.send(lorem);
-});
-
-router.get("/hb1", (req, res) => {
-  const message = "Bem-vindo(a) ao Web Academy!";
-  res.render("hb1", {
-    message,
-    layout: false,
-  });
-});
-
-router.get("/hb2", (req, res) => {
-  res.render("hb2", {
-    poweredByNodeJs: true,
-    name: "Express",
-    type: "Framework",
-    message: "A mensagem não será exibida!",
-    layout: false,
-  });
-});
-
-router.get("/hb3", (req, res) => {
-  const profs = [
-    { nome: "David Fernandes", sala: 1238 },
-    { nome: "Horácio Fernades", sala: 1233 },
-    { nome: "Edleno Moura", sala: 1236 },
-    { nome: "Elaine Harada", sala: 1231 },
-  ];
-  res.render("hb3", {
-    profs,
-    layout: false,
-  });
-});
-
-router.get("/hb4", (req, res) => {
-  const technologies = [
-    { name: "Express", type: "Framework", poweredByNodeJs: true },
-    { name: "Laravel", type: "Framework", poweredByNodeJs: false },
-    { name: "React", type: "Library", poweredByNodeJs: true },
-    { name: "Handlebars", type: "Engine View", poweredByNodeJs: true },
-    { name: "Django", type: "Framework", poweredByNodeJs: false },
-    { name: "Docker", type: "Virtualization", poweredByNodeJs: false },
-    { name: "Sequelize", type: "ORM tool", poweredByNodeJs: true },
-  ];
-  res.render("hb4", {
-    technologies,
-    layout: false,
-  });
-});
-
-router.use((req, res) => {
-  res.statusCode = 404;
-  res.send("Erro 404: Página não encontrada.");
-});
+router.get("/", MainController.index);
+router.get("/about", MainController.about);
+router.get("/bem-vindo/:nome", MainController.welcome);
+router.get("/lorem/:paragrafos", MainController.lorem);
+router.get("/hb1", MainController.hb1);
+router.get("/hb2", MainController.hb2);
+router.get("/hb3", MainController.hb3);
+router.get("/hb4", MainController.hb4);
+router.use(MainController.not_found);
 
 export default router;
