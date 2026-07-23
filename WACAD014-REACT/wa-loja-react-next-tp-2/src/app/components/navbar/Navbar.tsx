5641900ec7f185b87f2e32e4a54b06f71add5c9a
Aldemir Rodrigues da Silva <aldemir.rodrigues@gmail.com>
2026-07-22 20:44:45 -0400

    chore: adiciona exercício de exibição condicional da navbar.

--- WACAD014-REACT/wa-loja-react-next-tp-2/src/app/components/navbar/Navbar.tsx
+++ WACAD014-REACT/wa-loja-react-next-tp-2/src/app/components/navbar/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Navbar() {
+  const pathName = usePathname();
+  if (pathName === "/login" || pathName === "/register") return null;
   return (
     <nav className="navbar navbar-expand-md bg-light border-bottom border-body sticky-top">
       <div className="container-fluid">
