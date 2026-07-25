3060e2ac9a8700ab29a9b9eaebafacba0845e82b
Aldemir Rodrigues da Silva <aldemir.rodrigues@gmail.com>
2026-07-25 11:07:03 -0400

    chore: criação do diretório wa-loja-context-api-base-tp-2

---
+++ WACAD015-ESTADO/wa-loja-context-api-base-tp-2/src/app/components/FavoritesList/FavoritesList.tsx
@@ -0,0 +1,52 @@
+import { calculateDiscountedPrice } from "@/app/helpers";
+import FavoriteItem from "../FavoriteItem/FavoriteItem";
+import { useContext } from "react";
+import { FavoritesContext } from "@/app/contexts/FavoritesContext/FavoritesProvider";
+
+export default function FavoritesList() {
+  const { favorites } = useContext(FavoritesContext);
+  const totalFavoriteValue = favorites.reduce((acc, product) => {
+    return (
+      acc + calculateDiscountedPrice(Number(product.preco), product.desconto)
+    );
+  }, 0);
+
+  return (
+    <div className="card mb-4">
+      <div className="row card-body">
+        <h5 className="card-title mb-4 fw-bold">Lista de favoritos:</h5>
+
+        {favorites.length > 0 ? (
+          <div className="table-responsive">
+            <table className="table table-borderless">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Preço</th>
+                  <th>Desconto</th>
+                  <th>Opções</th>
+                </tr>
+              </thead>
+              <tbody>
+                {favorites.map((item) => (
+                  <FavoriteItem key={item.id} favoriteItem={item} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <p>Sua lista de favoritos está vazia.</p>
+        )}
+      </div>
+      <div className="card-footer d-flex flex-column">
+        <small className="text-muted">
+          Quantidade de produtos: {favorites.length}
+        </small>
+
+        <small className="text-muted">
+          Valor total: R$ {totalFavoriteValue}
+        </small>
+      </div>
+    </div>
+  );
+}
---
+++ WACAD015-ESTADO/wa-loja-context-api-base-tp-2/src/app/contexts/FavoritesContext/FavoritesProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Product } from "@/app/types/product";
+import { createContext, useState } from "react";
+
+type FavoritesContextType = {
+  favorites: Product[];
+  setFavorites: React.Dispatch<React.SetStateAction<Product[]>>;
+};
+
+export const FavoritesContext = createContext<FavoritesContextType>({
+  favorites: [],
+  setFavorites: () => {},
+});
+
+export const FavoritesProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [favorites, setFavorites] = useState<Product[]>([]);
+
+  const values = { favorites, setFavorites };
+
+  return (
+    <FavoritesContext.Provider value={values}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export default { FavoritesContext, FavoritesProvider };
